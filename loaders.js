fccce7a02fd2472bbddeeb4fcc3edb7a342cbc35
lilliarose <lillia.nelson18@gmail.com>
2023-03-19 18:28:10 +0000

    Update loaders.js
    update to use image URL rather than local stored images to reduce bandwith through github to keep under 1GB per month which may be exceeded if whole school use and also use at home.

--- loaders.js
+++ loaders.js
@@ -1,11 +1,20 @@
 // pug swing: https://media3.giphy.com/media/Kuw5gir5rTnaw/giphy.gif?cid=ecf05e47xen7u6crwn82xcam74il7kmocjcjg22187iys592&rid=giphy.gif&ct=g
 // giphy embed: <iframe src="https://giphy.com/embed/Kuw5gir5rTnaw" width="359" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dog-swing-Kuw5gir5rTnaw">via GIPHY</a></p>
 
+// mr Bean watch: https://media4.giphy.com/media/QBd2kLB5qDmysEXre9/giphy.gif?cid=ecf05e47uetloqnwgj5ljcnyoelxm6oc6nz4gd3evcwor2as&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/QBd2kLB5qDmysEXre9" width="480" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/time-mr-bean-look-at-the-QBd2kLB5qDmysEXre9">via GIPHY</a></p>
+
+// mouse swing: https://media0.giphy.com/media/mTRpdks4k5sgo/giphy.gif?cid=ecf05e476ej5ndqtmf3szi5z5p9kyso4by1on9y761cgqcl9&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/mTRpdks4k5sgo" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hamster-eternal-aprilsanimals-mTRpdks4k5sgo">via GIPHY</a></p>
+
 // dorry:  https://media4.giphy.com/media/dIW4s2cR6cvtu/giphy.gif?cid=ecf05e47nmxw4x521czru3utwz93nm447ssf4s0irm020nbq&rid=giphy.gif&ct=g
 // giphy embed: <iframe src="https://giphy.com/embed/UYbMnQghsqFr2" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/disneypixar-disney-pixar-dory-UYbMnQghsqFr2">via GIPHY</a></p>
 
 // dog fetch from UP: https://media2.giphy.com/media/IJZGS8c78XGU0/giphy.gif?cid=ecf05e47lrx5awxm6onblh3rx48o8xrgfldl913kr0ei8ouz&rid=giphy.gif&ct=g
 // giphy embed: <iframe src="https://giphy.com/embed/IJZGS8c78XGU0" width="480" height="271" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/disneypixar-disney-pixar-IJZGS8c78XGU0">via GIPHY</a></p>
+
+// dog from UP: https://media0.giphy.com/media/JMotSbZTCEWl2/giphy.gif?cid=ecf05e47j5ehzsilww0zexpqoi3znb7kcem1nn707na8c80t&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/JMotSbZTCEWl2" width="480" height="437" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cute-disney-dog-JMotSbZTCEWl2">via GIPHY</a></p>
 
 // running muppet: https://media1.giphy.com/media/7kn27lnYSAE9O/giphy.gif?cid=ecf05e475zbxm8ay1x9q48xxh2m6c297ae9skjllv08z2m29&rid=giphy.gif&ct=g
 // giphy embed:  <iframe src="https://giphy.com/embed/7kn27lnYSAE9O" width="480" height="373" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/running-muppets-7kn27lnYSAE9O">via GIPHY</a></p>
@@ -16,6 +25,9 @@
 // monstersInc: https://media0.giphy.com/media/MB5cRgSVDW4F2/giphy.gif?cid=ecf05e47yggg3y265z02afqa6gfl3re1x5klp3m76x75a8jl&rid=giphy.gif&ct=g
 // giphy embed: <iframe src="https://giphy.com/embed/MB5cRgSVDW4F2" width="480" height="345" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thegoodfilms-cinemagraph-art-film-MB5cRgSVDW4F2">via GIPHY</a></p>
 
+// giraffe:  https://media1.giphy.com/media/hFmIU5GQF18Aw/giphy.gif?cid=ecf05e47m3sr2d0lpmeugermejsa3fb3pypk2kxaok1thnwq&rid=giphy.gif&ct=g
+// giphy embed:<iframe src="https://giphy.com/embed/hFmIU5GQF18Aw" width="343" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/loop-problem-hFmIU5GQF18Aw">via GIPHY</a></p>
+
 // minions Gru: https://media0.giphy.com/media/lHJztSHlaX4NG/giphy.gif?cid=ecf05e47yggg3y265z02afqa6gfl3re1x5klp3m76x75a8jl&rid=giphy.gif&ct=g
 // giphy enbed: <iframe src="https://giphy.com/embed/lHJztSHlaX4NG" width="480" height="258" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/despicable-me-movie-lHJztSHlaX4NG">via GIPHY</a></p>
 
@@ -25,7 +37,19 @@
 // dancing orange: https://media4.giphy.com/media/l2JJHEW1lToNnJzxe/giphy.gif?cid=ecf05e476i3vtxwwzlu6h6bk5ebli8gwzpt5phw78ay9bd04&rid=giphy.gif&ct=g
 // giphy embed: <iframe src="https://giphy.com/embed/l2JJHEW1lToNnJzxe" width="460" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/playkids-children-junior-junioronthejob-l2JJHEW1lToNnJzxe">via GIPHY</a></p>
 
+// molang: https://media3.giphy.com/media/2lQCBjuFMLCOvXno4l/giphy.gif?cid=ecf05e47dcpsv22ccx0nr0d3khvyynz91bqyx13pt6jnm9wx&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/2lQCBjuFMLCOvXno4l" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/molangofficialpage-clap-bravo-2lQCBjuFMLCOvXno4l">via GIPHY</a></p>
 
+// cat music: https://media2.giphy.com/media/10YpWPBU7GAYwM/giphy.gif?cid=ecf05e470vowq750svzrleuqa2xq4nwax3x69ydj5a6ncqfy&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/10YpWPBU7GAYwM" width="375" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/headphones-cat-cute-10YpWPBU7GAYwM">via GIPHY</a></p>
+
+// socks: https://media3.giphy.com/media/d7ocTxquJN2JzUZf8s/giphy.gif?cid=ecf05e479xsqq3ewkh9x0x16e794xkl3tx435hs9awmhdq5w&rid=giphy.gif&ct=g
+// giphy embd: <iframe src="https://giphy.com/embed/d7ocTxquJN2JzUZf8s" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-loop-socks-d7ocTxquJN2JzUZf8s">via GIPHY</a></p>
+
+// cars: https://media3.giphy.com/media/Mxsc4ZpZBEBdm/giphy.gif?cid=ecf05e47fgy56zxic7llo83kdyw3occeahng7zogtwrqad6x&rid=giphy.gif&ct=g
+// giphy embed: <iframe src="https://giphy.com/embed/Mxsc4ZpZBEBdm" width="480" height="194" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/disneypixar-disney-pixar-Mxsc4ZpZBEBdm">via GIPHY</a></p>
+
+/*
 const loaderSource = ['pupswing.gif',
 'MrBeanTime.gif',
 'mouseSwing.gif',
@@ -43,6 +67,25 @@ const loaderSource = ['pupswing.gif',
 'molang.gif',
 'catMusic.gif', 
 'socks.gif'];
+*/
+
+const loaderSource = ['https://media3.giphy.com/media/Kuw5gir5rTnaw/giphy.gif?cid=ecf05e47xen7u6crwn82xcam74il7kmocjcjg22187iys592&rid=giphy.gif&ct=g',  // pup swing
+                    'https://media4.giphy.com/media/QBd2kLB5qDmysEXre9/giphy.gif?cid=ecf05e47uetloqnwgj5ljcnyoelxm6oc6nz4gd3evcwor2as&rid=giphy.gif&ct=g', // Mr Bean watch
+                    'https://media0.giphy.com/media/mTRpdks4k5sgo/giphy.gif?cid=ecf05e476ej5ndqtmf3szi5z5p9kyso4by1on9y761cgqcl9&rid=giphy.gif&ct=g', // hampster swinging
+                    'https://media4.giphy.com/media/l2JJHEW1lToNnJzxe/giphy.gif?cid=ecf05e476i3vtxwwzlu6h6bk5ebli8gwzpt5phw78ay9bd04&rid=giphy.gif&ct=g', // dancing orange
+                    'https://media2.giphy.com/media/IJZGS8c78XGU0/giphy.gif?cid=ecf05e47lrx5awxm6onblh3rx48o8xrgfldl913kr0ei8ouz&rid=giphy.gif&ct=g', // dog from up fetch
+                    'https://media1.giphy.com/media/7kn27lnYSAE9O/giphy.gif?cid=ecf05e475zbxm8ay1x9q48xxh2m6c297ae9skjllv08z2m29&rid=giphy.gif&ct=g', // running muppet
+                    'https://media1.giphy.com/media/VTjMuV5o8r4eA/giphy.gif?cid=ecf05e47yfrq7sj4a79mebf4k7w17s1hgp88s14ajot1lrg9&rid=giphy.gif&ct=g', // sloth
+                    'https://media3.giphy.com/media/bCcxY1ADkAqfS/giphy.gif?cid=ecf05e47zg0tsmbrn41w6exrdr6u4dldtscu3pgeprycgnmn&rid=giphy.gif&ct=g', // snow white
+                    'https://media0.giphy.com/media/MB5cRgSVDW4F2/giphy.gif?cid=ecf05e47yggg3y265z02afqa6gfl3re1x5klp3m76x75a8jl&rid=giphy.gif&ct=g', // monsters inc
+                    'https://media1.giphy.com/media/hFmIU5GQF18Aw/giphy.gif?cid=ecf05e47m3sr2d0lpmeugermejsa3fb3pypk2kxaok1thnwq&rid=giphy.gif&ct=g', // giraffe
+                    'https://media4.giphy.com/media/dIW4s2cR6cvtu/giphy.gif?cid=ecf05e47nmxw4x521czru3utwz93nm447ssf4s0irm020nbq&rid=giphy.gif&ct=g', // dory
+                    'https://media0.giphy.com/media/JMotSbZTCEWl2/giphy.gif?cid=ecf05e47j5ehzsilww0zexpqoi3znb7kcem1nn707na8c80t&rid=giphy.gif&ct=g', // dog from UP
+                    'https://media0.giphy.com/media/lHJztSHlaX4NG/giphy.gif?cid=ecf05e47yggg3y265z02afqa6gfl3re1x5klp3m76x75a8jl&rid=giphy.gif&ct=g', // minions Gru
+                    'https://media3.giphy.com/media/2lQCBjuFMLCOvXno4l/giphy.gif?cid=ecf05e47dcpsv22ccx0nr0d3khvyynz91bqyx13pt6jnm9wx&rid=giphy.gif&ct=g', // molang
+                    'https://media2.giphy.com/media/10YpWPBU7GAYwM/giphy.gif?cid=ecf05e470vowq750svzrleuqa2xq4nwax3x69ydj5a6ncqfy&rid=giphy.gif&ct=g', // cat music
+                    'https://media3.giphy.com/media/d7ocTxquJN2JzUZf8s/giphy.gif?cid=ecf05e479xsqq3ewkh9x0x16e794xkl3tx435hs9awmhdq5w&rid=giphy.gif&ct=g', // socks up down
+                    'https://media3.giphy.com/media/Mxsc4ZpZBEBdm/giphy.gif?cid=ecf05e47fgy56zxic7llo83kdyw3occeahng7zogtwrqad6x&rid=giphy.gif&ct=g'] // cars
 
 let loaderLength = loaderSource.length
 let loaderOrder = [];
@@ -53,21 +96,19 @@ function setLoaderOrder() {
     for (let i = 0; i < loaderLength; i++) {
         loaderOrder.push(i);
     }
-
     loaderOrder = randomShuffle(loaderOrder);
-
 }
 
 function setRandomLoader() {
-
     if (!randomOrderSet){
         setLoaderOrder()
         randomOrderSet = true;
     }
     let currentLoader = loaderSource[loaderOrder[loaderCount%loaderLength]];
-    //console.log(currentLoader)
+    console.log(loaderOrder)
+    console.log(loaderOrder[loaderCount%loaderLength])
     let loaderImage = document.getElementById("loaderImage");
-    loaderImage.src = 'Gifs/'+currentLoader;
+    loaderImage.src = currentLoader;
     loaderCount++;
 }
 
